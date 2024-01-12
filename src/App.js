135a6335e53a91a83db688079245198543d2b559
aniWakalkar <aniket.wakalkar@caredose.com>
2024-01-12 16:11:59 +0530

    Mistakes makes us perfect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from "./components/Home";
+import Details from "./components/Details";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-      <h1>HELLO WORLD</h1>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}/>
+          <Route path="/details" element={<Details />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Router>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
